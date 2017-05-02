025e9668e7d1178f3d8a05996d56e2fc4fe47acf
webnduck <maria.hardy@numericable.fr>
2017-05-02 02:13:51 +0200

    1er commit à la création du repo local

---
+++ GitTest/src/FirstPackage/MaClasse_B.java
@@ -0,0 +1,10 @@
+package FirstPackage;
+
+public class MaClasse_B {
+
+	String msg;
+	int i;
+	
+	String msg2;
+	int iiii;
+}
---
+++ GitTest/src/SecondPackage/TestConsole.java
@@ -0,0 +1,18 @@
+package SecondPackage;
+
+import FirstPackage.MaClasse_A;
+import FirstPackage.MaClasse_B;
+
+public class TestConsole {
+
+	public static void main(String[] args) {
+
+		MaClasse_A a = new MaClasse_A();
+		MaClasse_B b = new MaClasse_B();
+		
+		System.out.println(a.toString());
+		System.out.println(b.toString());
+		System.out.println("\nAjouté par Maria le 02/05/17 à 02h11 : Le projet ne s'importe pas, c'est moins déséspérant... en chantant :)");
+	}
+
+}
