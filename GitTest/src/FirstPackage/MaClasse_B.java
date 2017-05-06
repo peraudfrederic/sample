649b08dedd6dffd4d2a649ff7053074274778e87
Jean Vladimir TWAHIRWA <tvradmir@gmail.com>
2017-05-06 13:47:06 +0200

    commit2 vlado

--- GitTest/src/FirstPackage/MaClasse_B.java
+++ GitTest/src/FirstPackage/MaClasse_B.java
@@ -7,7 +7,7 @@ public class MaClasse_B {
 	String msg;
 	int i;
 	
-	String msg23;
+	String msg06;
 	int iiii;
 	Date aujourdhui = new Date(); // modif vlado
 	String ii3; // changement pour le test du 040517 à 14h05
